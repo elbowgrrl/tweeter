c2899e023a6304c297e7445a63884d9d2616df5e
elbowgrrl <76022369+elbowgrrl@users.noreply.github.com>
2021-06-15 18:39:35 -0700

    Adding character counter functionality.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+// console.log("char counter");
+
+$(document).ready(function() {
+  
+  $("#tweet-text").on("input", function(event) {
+    const $inputCounter = $(this).val();
+    let $charCounter = $(this).parent().children(".button-counter").children("#char-counter");
+    const $count = $charCounter.html(140 - $inputCounter.length);
+    if ($inputCounter.length > 140) {
+      $charCounter.addClass('over-count');
+    }
+  });
+
+});
