e2052bd3de2582b9cf1f10764fb905f847c0ded0
elbowgrrl <76022369+elbowgrrl@users.noreply.github.com>
2021-06-16 21:35:04 -0700

    adding ajax calls to load tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,66 +4,75 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const tweetData = require('..initial-tweets.json'); //???????
-// console.log("tweetData", tweetData);
 
-
-
-
-
-$(document).ready(function() {
-
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+$(document).ready(function () {
+  //Utilizes the create tweet function to render all tweets in data set
+  const renderTweets = function (tweets) {
+    const $targetContainer = $("#tweet-container");
+    let $tweet;
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      $tweet = createTweetElement(tweet);
+      $targetContainer.prepend($tweet);
     }
-  ]
-
-  const renderTweets = function(tweets) {
-  const $targetContainer = $('#tweet-container');
-  let $tweet;
-  // loops through tweets
-  for (const tweet of tweets) {
-  // calls createTweetElement for each tweet
-  $tweet = createTweetElement(tweet);
-  $targetContainer.append($tweet);
-  }
-  // takes return value and appends it to the tweets container
-  return;
+    // takes return value and appends it to the tweets container
+    return;
   };
 
+  //returns a dynamic html elemt based on Tweet data
   function createTweetElement(data) {
-    
+    const $content = $().text(data.content.text);///maybe not the best solution
     let $tweet = $(`<article class="tweet-container">
-    <header class="tweet-container"><img src=${data.user.avatars}/>${data.user.name}<span>${data.user.handle}</span></header>
-    <p>${data.content.text}</p>
-    <footer class="tweet-container">${timeago.format(data.created_at)}<span id="icons"><i class="fas fa-flag"></i><i class="fas fa-heart"></i><i class="fas fa-retweet"></i></span></footer>
+    <header class="tweet-container"><img src=${data.user.avatars}/>${
+      data.user.name
+    }<span>${data.user.handle}</span></header>
+    <p>${$content}</p>
+    <footer class="tweet-container">${timeago.format(
+      data.created_at
+    )}<span id="icons"><i class="fas fa-flag"></i><i class="fas fa-heart"></i><i class="fas fa-retweet"></i></span></footer>
     </article>`);
-  
+
     return $tweet;
+  }
+
+  //makes a request to /tweets and receive the array of tweets as JSON
+  const loadTweets = function () {
+    $.ajax({ url: "/tweets/", method: "GET" }).then((tweets) => {
+      renderTweets(tweets);
+    });
   };
 
-  renderTweets(tweetData)
+  loadTweets();
 
+  //Uses Ajax to capture data from tweet form and sends to server
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    const $data = $(this).serialize();
+    console.log("this", $data)
+    $("#tweet-text").val("");
+    $("#tweet-text").trigger("input");
+    
+    const inputTextLength = $data.length - 5;
 
+    if (inputTextLength <= 0 || $data === null) {
+      alert("Please enter a tweet");
+      return;
+    }
+
+    if (inputTextLength >= 140) {
+      alert("Please enter a tweeet 140 char or less");
+      return;
+    }
+
+    $.ajax({
+      url: "/tweets/",
+      method: "POST",
+      data: $data,
+      
+    }).then(() => {
+      loadTweets();
+    });
+    
+  });
 });
-
